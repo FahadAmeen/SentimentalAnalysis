4f84a759e7ecdfc07332b820576f1d97c8d8bc92
Fahad Ameen <mfah@ciklum.com>
2020-04-18 19:08:04 +0500

    write in excel file

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import random
 import re
 import string
 
+from nltk.tokenize import word_tokenize
 from nltk import classify, NaiveBayesClassifier
 from nltk.corpus import twitter_samples, stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tag import pos_tag
+
+import xlsxwriter
 
 
 def remove_noise(tweet_tokens, stop_words=()):
@@ -47,6 +50,38 @@ def get_predicted_results(classifier, test_data):
             prediction = classifier.classify(dict([token, True] for token in tweet))
             predicted_results.append(prediction)
     return predicted_results
+
+
+def write_in_excelsheet():
+    workbook = xlsxwriter.Workbook('SentimentAnalysis.xlsx')
+    worksheet = workbook.add_worksheet('Twitter dataset Predictions')
+    cell_format = workbook.add_format({'bold': True, 'font_color': 'red'})
+    cell_format_sum = workbook.add_format({'bold': True, 'font_color': 'blue'})
+    cell_format_accuracy = workbook.add_format({'bold': True})
+
+    worksheet.write('A1', 'Tweets', cell_format_accuracy)
+    worksheet.write('B1', 'Actual Results', cell_format_accuracy)
+    worksheet.write('C1', 'Predicted Results', cell_format_accuracy)
+    row = 1
+    wrong_predictions = 0
+    for i in range(3000):
+        col = 0
+        worksheet.write(row, col, tweets[i])
+        col = 1
+        worksheet.write(row, col, actual_test_results[i])
+        col = 2
+        if actual_test_results[i] != predicted_results[i]:
+            wrong_predictions += 1
+            worksheet.write(row, col, predicted_results[i], cell_format)
+        else:
+            worksheet.write(row, col, predicted_results[i])
+        row += 1
+    col = 2
+    row += 1
+    worksheet.write(row, col, wrong_predictions, cell_format_sum)
+    col = 1
+    worksheet.write(row, col, "Accuracy:" + str(accuracy), cell_format_sum)
+    workbook.close()
 
 
 # downloaded the twitter dataset
@@ -92,17 +127,20 @@ test_data = dataset[7000:]
 # used the NaiveBayes classifier and trained it with training set
 classifier = NaiveBayesClassifier.train(train_data)
 accuracy = classify.accuracy(classifier, test_data)
-print("Accuracy is:", accuracy)
+# print("Accuracy is:", accuracy)
 predicted_results = get_predicted_results(classifier, test_data)
-print("Predicted results", predicted_results)
+# print("Predicted results", predicted_results)
+
 tweets = []
 actual_test_results = []
+# joined the sentences
 for tweet in test_data:
     tweet_sentence = ' '.join([word for word in tweet[0]])
     tweets.append(tweet_sentence)
     actual_test_results.append(tweet[1])
 
+write_in_excelsheet()
 # Custom testing example
-# custom_tweet = "I ordered just once from Subway, they screwed up, never used the app again."
-# custom_tokens = remove_noise(word_tokenize(custom_tweet))
-# print(custom_tweet, classifier.classify(dict([token, True] for token in custom_tokens)))
+custom_tweet = "I ordered just once from Subway, they screwed up, never used the app again."
+custom_tokens = remove_noise(word_tokenize(custom_tweet))
+print(custom_tweet, classifier.classify(dict([token, True] for token in custom_tokens)))
